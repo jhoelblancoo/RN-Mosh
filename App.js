112ec30124a406a49a8e0e24b2b635ded38e3711
jhoelblancoo <jhoel.blanco@correo.unimet.edu.ve>
2021-09-25 11:37:53 -0400

    1:18hr , mitad del curso hasta status bar height

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Text pressed");
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on 333your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={4} onPress={handlePress}>
+        Open up App.js to start working on
+        3aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa33your app!
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Image tap")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        color="green"
+        title="tocate, tocate tocate "
+        onPress={() =>
+          Alert.alert("Ra rauw", "esta noche pol video", [
+            { text: "Pendiente", onPress: () => console.log("wisin") },
+            { text: "que te ", onPress: () => console.log("yandel") },
+          ])
+        }
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10 : 0,
+    // justifyContent: "center",
+    // alignItems: "center",
   },
 });
